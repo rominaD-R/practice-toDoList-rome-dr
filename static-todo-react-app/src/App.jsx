cc6199885f335a48ac64b431409357f79ec83c74
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-02-10 19:16:01 -0600

    Made react todo version

--- static-todo-react-app/src/App.jsx
+++ static-todo-react-app/src/App.jsx
@@ -1,23 +1,18 @@
 import './App.css'
 import './components/ToDoList'
+import Header from './components/Header'
 import ToDoList from './components/ToDoList'
 
 function App() {
 
   return (
     <>
-      <h1>ToDo List assignment</h1>
       <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Header />
       </div>
       <div>
         <ToDoList />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- static-todo-react-app/src/components/ToDoList.jsx
+++ static-todo-react-app/src/components/ToDoList.jsx
@@ -1,7 +1,11 @@
 export default function ToDoList() {
    return (
        <div>
-           <p>todo list component!</p>
+            <ul>
+                <li><input type="checkbox" name="task1" id="task1" /> Read my book</li>
+                <li><input type="checkbox" name="task2" id="task2" /> Clean my room</li>
+                <li><input type="checkbox" name="task3" id="task1" /> Walk a mile in the park</li>
+            </ul>
        </div>
    );
 }
